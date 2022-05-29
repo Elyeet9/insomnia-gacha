69e248f25a523fecfc3979e5b96f60e982216c18
Elyeet9 <goyasleonardo@gmail.com>
2022-05-29 01:00:17 -0500

    discordjs 13 finish

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
 
 const gacha = require('./gacha');
 
